f108c0e10060fc612c7dae010271c707e9b33434
bjw <lelouch0823@hotmail.com>
2016-12-21 17:31:47 +0800

    ddd

--- app/src/main/java/com/example/bujiawei/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bujiawei/myapplication/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Double.parseDouble("dd");
+        Double.parseDouble("d");
     }
 }
