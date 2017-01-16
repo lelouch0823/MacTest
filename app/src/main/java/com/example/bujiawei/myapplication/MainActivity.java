660100e44b7f459a239322fd5ab0d7a00424f208
bjw <lelouch0823@hotmail.com>
2017-01-16 14:36:35 +0800

    rrrrr

--- app/src/main/java/com/example/bujiawei/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bujiawei/myapplication/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Double.parseDouble("dhadahkdh");
+
+
+
+        Double.parseDouble("");
     }
 }
